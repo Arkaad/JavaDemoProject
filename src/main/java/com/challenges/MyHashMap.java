e619940071a42e1b9a7f9d3bbe96b1d736e26d59
Arka Dutta <arka92ad@gmail.com>
2020-11-29 17:26:43 +0530

    specifying java version in pom.xml

---
+++ src/main/java/com/challenges/MyHashMap.java
@@ -0,0 +1,49 @@
+package com.challenges;
+
+public class MyHashMap<K, V> {
+
+    Object[] array;
+
+    MyHashMap() {
+        array = new Object[1000];
+    }
+
+    void put(K key, V value) {
+        int index = key.hashCode();
+        array[index] = value;
+    }
+
+    V get(K key) {
+        return (V) array[key.hashCode()];
+    }
+
+
+    public static void doTestsPass() {
+        int[][] testCases = {{1, 2}, {3, 4}, {5, 6}};
+        boolean passed = true;
+
+        MyHashMap<Integer, Integer> map = new MyHashMap<>();
+
+        for (int test[] : testCases) {
+            Integer key = test[0];
+            Integer value = test[1];
+
+            map.put(key, value);
+
+            if (!value.equals(map.get(key))) {
+                System.out.println("test Failed[" + key + "," + value + "]");
+                passed = false;
+
+            }
+        }
+
+        if (passed) {
+            System.out.println("All test passed");
+        }
+    }
+
+    public static void main(String[] args) {
+        doTestsPass();
+    }
+}
+
