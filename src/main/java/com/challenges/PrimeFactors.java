c73576effc861b9f1572e1966fdf2d143621686d
arkaad <arka92ad@gmail.com>
2020-11-30 20:57:57 +0530

    Minor changes

--- src/main/java/com/challenges/PrimeFactors.java
+++ src/main/java/com/challenges/PrimeFactors.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class PrimeFactors {
     public static List<Integer> primeFactors(int number) {
         int n = number;
-        List<Integer> factors = new ArrayList<Integer>();
+        List<Integer> factors = new ArrayList<>();
         for (int i = 2; i <= n; i++) {
             while (n % i == 0) {
                 factors.add(i);
@@ -17,15 +17,15 @@ public class PrimeFactors {
     }
 
     public static void main(String[] args) {
-        System.out.println("Primefactors of 6");
+        System.out.println("Prime factors of 6");
         for (Integer integer : primeFactors(6)) {
             System.out.println(integer);
         }
-        System.out.println("Primefactors of 5");
+        System.out.println("Prime factors of 5");
         for (Integer integer : primeFactors(5)) {
             System.out.println(integer);
         }
-        System.out.println("Primefactors of 12");
+        System.out.println("Prime factors of 12");
         for (Integer integer : primeFactors(12)) {
             System.out.println(integer);
         }
